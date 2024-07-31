59731b72706d1f7e92fa972ca65bbded291d1457
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-31 12:22:56 -0700

    Update page.tsx

--- app/(main)/guestbook/page.tsx
+++ app/(main)/guestbook/page.tsx
@@ -6,9 +6,9 @@ import { fetchGuestbookMessages } from '~/db/queries/guestbook'
 
 import { Guestbook } from './Guestbook'
 
-const title = '留言墙'
+const title = 'Noteworthy'
 const description =
-  '在这里，你可以留下你想对我说的话，或是你的建议，或是你的想法，或是你的批评，或是你的赞美，或是你的鼓励，或是你的吐槽。'
+  'I have worked on industrial during my full-time job and research projects during my PhD from the following partners'
 export const metadata = {
   title,
   description,
@@ -30,7 +30,7 @@ export default async function GuestBookPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          欢迎来到我的留言墙
+          Noteworthy
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
