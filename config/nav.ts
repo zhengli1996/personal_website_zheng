2efb2ed1c99aaa2e10d937555914f71728cb2a29
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-31 12:34:47 -0700

    Update nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -1,8 +1,8 @@
 export const navigationItems = [
-  { href: '/', text: '首页' },
-  { href: '/blog', text: '博客' },
-  { href: '/projects', text: '项目' },
-  { href: '/guestbook', text: '留言墙' },
-  { href: '/ama', text: 'AMA 咨询' },
+  { href: '/', text: 'Z' },
+  { href: '/guestbook', text: 'H' },
+  { href: '/projects', text: 'E' },
+  { href: '/blog', text: 'N' },
+  { href: '/ama', text: 'G' },
   // { href: '/about', text: '关于' },
 ]
