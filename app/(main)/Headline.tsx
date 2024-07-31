d19fdff778b551752c5b69b9e9ef26d9cf585e71
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-30 17:47:20 -0700

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
 import { SparkleIcon, UserSecurityIcon } from '~/assets'
-import { PeekabooLink } from '~/components/links/PeekabooLink'
+
 import { SocialLink } from '~/components/links/SocialLink'
 
 function Developer() {
