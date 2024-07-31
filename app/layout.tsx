22b93c2bf5359f3559054a933c9dd96915eed53d
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-31 13:25:30 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ import { seo } from '~/lib/seo'
 export const metadata: Metadata = {
   metadataBase: seo.url,
   title: {
-    template: '%s | Cali Castle',
+    template: '%s | Zheng Li',
     default: seo.title,
   },
   description: seo.description,
-  keywords: 'Cali,Cali Castle,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
+  keywords: 'Zheng',
   manifest: '/site.webmanifest',
   robots: {
     index: true,
@@ -34,10 +34,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: {
       default: seo.title,
-      template: '%s | Cali Castle',
+      template: '%s | Zheng Li',
     },
     description: seo.description,
-    siteName: 'Cali Castle',
+    siteName: 'Zheng Li',
     locale: 'zh_CN',
     type: 'website',
     url: 'https://cali.so',
