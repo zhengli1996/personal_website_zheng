8c1d5a39ca5715819c14831e141553e1af7782bd
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-31 16:44:53 -0700

    Update nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -1,6 +1,7 @@
 export const navigationItems = [
   { href: '/', text: 'Z' },
   { href: '/guestbook', text: 'H' },
+  { href: '/projects', text: 'H' },
   { href: '/projects', text: 'E' },
   { href: '/blog', text: 'N' },
   { href: '/ama', text: 'G' },
