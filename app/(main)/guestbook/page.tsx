fa4e5047677bcdb9039ce5e8e4eeedbeaafc7721
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-31 12:39:02 -0700

    Update page.tsx

--- app/(main)/guestbook/page.tsx
+++ app/(main)/guestbook/page.tsx
@@ -6,9 +6,9 @@ import { fetchGuestbookMessages } from '~/db/queries/guestbook'
 
 import { Guestbook } from './Guestbook'
 
-const title = 'Noteworthy'
+const title = 'Hobbies'
 const description =
-  'I have worked on industrial during my full-time job and research projects during my PhD from the following partners'
+  'I am passionate to explore new things and all possibilities. I like photograhy, swimming, diving, cooking, coffee, Japanse whiskey, gym training, house and trance music. '
 export const metadata = {
   title,
   description,
@@ -30,7 +30,7 @@ export default async function GuestBookPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Noteworthy
+          Hobbies
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
