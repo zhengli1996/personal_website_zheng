4e2780b0b51e06e80eb332e90c65353668cd7453
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-30 19:06:48 -0700

    Update Footer.tsx

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -116,10 +116,7 @@ export async function Footer() {
             </div>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
-                &copy; {new Date().getFullYear()} Cali Castle. 网站已开源：
-                <PeekabooLink href="https://github.com/CaliCastle/cali.so">
-                  GitHub
-                </PeekabooLink>
+                &copy; {new Date().getFullYear()} Zheng Li.
               </p>
               <Links />
             </div>
