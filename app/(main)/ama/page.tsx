c761b129f703b020c0c8f38b07797d5accf7a897
zhengli1996 <connor.zheng.li@gmail.com>
2024-07-30 18:52:11 -0700

    Update page.tsx

--- app/(main)/ama/page.tsx
+++ app/(main)/ama/page.tsx
@@ -8,9 +8,9 @@ import AlipayQR from './alipay-qr.jpg'
 import ThankYouLetterScreenshot1 from './Arc aagD26w9@2x.png'
 import ThankYouLetterScreenshot2 from './Arc ynleUdHy@2x.png'
 
-const title = 'AMA 一对一咨询'
+const title = 'Gallery'
 const description =
-  'Cali 提供一对一的咨询服务（Ask Me Anything）。我有前端开发、全栈开发、UI/UX 设计、创业经验、英语技能、美国留学与工作、内容创作等经验，可以为你解答相关的问题，我也支持纯英语沟通。'
+  'Myself, my daily, my photography'
 
 export const metadata = {
   title,
@@ -31,7 +31,7 @@ export default function AskMeAnythingPage() {
     <Container className="mt-16 sm:mt-24">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Ask Me Anything / 一对一咨询
+          Gallery
         </h1>
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
